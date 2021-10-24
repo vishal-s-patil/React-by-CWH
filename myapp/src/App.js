fc6b6dbcdb66bf7301c4741935052f5acd9f0950
vishal-s-patil <vishalspolicepatil55@gmail.com>
2021-10-24 20:07:04 +0530

    updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,12 +2,28 @@
 import './App.css';
 import TextUtils from './TextUtiles/TextUtils';
 import DarkMode from './DarkMode/DarkMode';
+import react, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const changeMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }
+  }
+
   return (
     <>
-    {/* <TextUtils/> */}
-    <DarkMode/>
+    <TextUtils changeMode={changeMode} mode={mode}/>
+    {/* <DarkMode/> */}
     </>
   );
 }
--- myapp/src/TextUtiles/TextUtils.js
+++ myapp/src/TextUtiles/TextUtils.js
@@ -1,37 +1,77 @@
 import react, {useState} from "react";
 import './Textutilscss.css'
 
-export default function TextUtils()
+export default function TextUtils(props)
 {
+    const [myStyle, setStyle] = useState({
+        color : 'black',
+        backgroundColor : 'white'
+    })
+
+    const handleModeBtn = ()=>{
+        if (props.mode === 'light'){
+            setStyle({
+                color : 'white',
+                backgroundColor : 'black'
+            })
+        }
+        else{
+            setStyle({
+                color : 'black',
+                backgroundColor : 'white'
+            })
+        }
+    }
+
     let handleUpperCase = () => {
-        console.log("upper");
+        // console.log("upper");
         let newStr = text.toUpperCase();
         setText(newStr);
     }
 
     let handleLowerCase = () => {
-        console.log("Lower");
+        // console.log("Lower");
         let newStr = text.toLowerCase();
         setText(newStr);
     }
 
+    let handleExtraSpaces = () => {
+        let splitted = text.split(/[ ]+/);
+        setText(splitted.join(" "))
+    }
+
+    let handleCopy = () => {
+        let textToSelect = document.getElementsByClassName("textArea")[0];
+        textToSelect.select();
+        navigator.clipboard.writeText(textToSelect.value);
+    }
+
     let handleOnChange = (event) => {
-        console.log("On Change");
+        // console.log("On Change");
         setText(event.target.value)
     }    
 
     const [text, setText] = useState("")
     return (
         <>
-        <div className="container">
-        <textarea cols="30" rows="10" value={text} onChange={handleOnChange} className="text-area"></textarea>
+        <div className="container" style={myStyle}>
+            <textarea cols="30" rows="10" value={text} onChange={handleOnChange} className="textArea" style={myStyle}></textarea>
 
-        <button onClick={handleUpperCase} className="upper-btn">convert to upper case </button>
+            <button onClick={handleUpperCase} className="btnn" style={myStyle}>convert to upper case</button>
 
-        <button onClick={handleLowerCase} className="lower-btn">convert to upper case </button>
+            <button onClick={handleLowerCase} className="btnn" style={myStyle}>convert to lower case </button>
 
-        <p>word count : {text.split(" ").length}</p>
-        <p>characters count : {text.length}</p>
+            <button onClick={handleExtraSpaces} className="btnn" style={myStyle}>Remove Extra Spaces</button>
+
+            <button onClick={handleCopy} className="btnn" style={myStyle}>copy text</button>
+
+            <div className="form-check form-switch">
+                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.changeMode} onChange={handleModeBtn}/>
+                <label className="form-check-label" HTMLFor="flexSwitchCheckDefault">Enable dark mode</label>
+            </div>
+
+            <p>word count : {text.split(" ").length}</p>
+            <p>characters count : {text.length}</p>
         </div>
         </>
     )
