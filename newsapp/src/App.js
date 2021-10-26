f90ff0b785f6d75e8ba0cb81d5c3b56a6fedacb2
vishal-s-patil <vishalspolicepatil55@gmail.com>
2021-10-27 01:24:37 +0530

    initial commit

---
+++ newsapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import News from './News/News';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
+    )
+  }
+}
+
