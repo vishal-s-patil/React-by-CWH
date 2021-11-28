402c8b73b7c6bef5f3a550cf8b3255192c426dae
vishal-s-patil <vishalspolicepatil55@example.com>
2021-11-28 14:46:51 +0530

    added next and previous to news app

--- newsapp/src/News/News.js
+++ newsapp/src/News/News.js
@@ -5,18 +5,47 @@ export default class News extends Component {
     constructor(){
         super();
         this.state = {
-            articles : []
+            articles : [],
+            loading : false,
+            page : 1
         }
     }
     
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=af4400b4baae40f8a4bb5a1cd09ea853&page=2";
+        let url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=af4400b4baae40f8a4bb5a1cd09ea853&page=${this.state.page}&pageSize=30`;
         let data = await fetch(url);
         let parsedData = await data.json();
         this.setState({
-            articles : parsedData.articles
+            articles : parsedData.articles,
+            totalResults : parsedData.totalResults
         })
         console.log(this.state.articles);
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=af4400b4baae40f8a4bb5a1cd09ea853&page=${this.state.page - 1}&pageSize=30`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles : parsedData.articles,
+            page : this.state.page - 1
+        })
+    }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults/30)){
+
+        }
+        else
+        {
+            let url = `https://newsapi.org/v2/top-headlines?category=business&country=in&apiKey=af4400b4baae40f8a4bb5a1cd09ea853&page=${this.state.page + 1}&pageSize=30`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles : parsedData.articles,
+                page : this.state.page + 1
+            })
+        }
     }
 
     render() {
@@ -28,11 +57,15 @@ export default class News extends Component {
                     this.state.articles.map((element) => {
                         console.log("hello");
                         return (<div className="col-md-3 mx-3 my-3" key={element.url}>
-                                <NewsItem title={element.title?element.title:""} disc={element.description?element.description:""} imgUrl={element.urlToImage} url={element.url}/>
+                                <NewsItem title={element.title?element.title:""} disc={element.description?element.description:""} imgUrl={element.urlToImage?element.urlToImage:"https://banner2.cleanpng.com/20180401/yfw/kisspng-zee-punjabi-television-channel-zee-entertainment-e-punjab-5ac0ad443c2319.6982965215225767082463.jpg"} url={element.url}/>
                                 </div>)
                     }
                     )
                 } 
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" class="btn btn-light" onClick={this.handlePrevClick}> &larr; previous</button>
+                    <button type="button" class="btn btn-light" onClick={this.handleNextClick}>next &rarr;</button>
                 </div>
 
             </div>
--- newsapp/src/News/NewsItem/NewsItem.js
+++ newsapp/src/News/NewsItem/NewsItem.js
@@ -7,16 +7,18 @@ export default class NewsItem extends Component {
     render() {
         let {title, disc, imgUrl, url} = this.props;
         return (
-            <div>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5> 
-                        <p className="card-text">{disc}</p>
-                        <a href={url} target="_blank" className="btn btn-primary">Read more</a>
+            <>
+                <div>
+                    <div className="card" style={{width: "18rem"}}>
+                        <img src={imgUrl} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{title}</h5> 
+                            <p className="card-text">{disc}</p>
+                            <a href={url} target="_blank" className="btn btn-primary">Read more</a>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
